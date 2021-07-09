0ad8262dfe074840ec9589bbdd4867b56fb98fc8
錢永炘 <qianyongxin@qianyongxindeMacBook-Pro.local>
2021-07-09 17:01:27 +0800

    complete users

--- app/src/main/java/neu/edu/madcourse/firebasechatapp/MessageActivity.java
+++ app/src/main/java/neu/edu/madcourse/firebasechatapp/MessageActivity.java
@@ -1,14 +1,123 @@
 package neu.edu.madcourse.firebasechatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+
+import neu.edu.madcourse.firebasechatapp.Model.Users;
 
 public class MessageActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView username;
+    ImageView imageView;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference ref;
+
+    Intent intent;
+
+    RecyclerView messageRecyclerview;
+    EditText messageEditText;
+    ImageButton messageSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        username = findViewById(R.id.message_activity_username);
+        imageView = findViewById(R.id.message_activity_profile_img);
+        toolbar = findViewById(R.id.message_activity_toolbar);
+
+        intent = getIntent();
+        String userId = intent.getStringExtra("userId");
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        ref = FirebaseDatabase.getInstance().getReference("MyUsers").child(userId);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                Users user = snapshot.getValue(Users.class);
+                assert user != null;
+                username.setText(user.getUsername());
+                imageView.setImageResource(R.mipmap.ic_launcher);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        messageSendButton = findViewById(R.id.message_activity_send_button);
+        messageEditText = findViewById(R.id.message_activity_message_edit_text);
+
+        messageSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = messageEditText.getText().toString();
+                if (message.length() > 0) {
+                    sendMessage(firebaseUser.getUid(), userId, message);
+                } else {
+                    Toast.makeText(MessageActivity.this, "failed", Toast.LENGTH_LONG).show();
+                }
+                messageEditText.setText("");
+            }
+        });
+    }
+
+    private void sendMessage(String sender, String receiver, String message) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("sender", sender);
+        map.put("receiver", receiver);
+        map.put("message", message);
+
+        ref.child("Chats").push().setValue(map);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MessageActivity.this, LoginActivity.class));
+                finish();
+                return true;
+        }
+        return false;
     }
 }
